3ceaad394e1228de56bf73172e5350d57ed53264
ayeshazaffar <sp23-bse-048@cuilahore.edu.pk>
2024-10-22 16:16:35 +0500

    mid

---
+++ mid.js
@@ -0,0 +1,11 @@
+// Function to show the introduction box when hovering on the image
+function showIntro() {
+   
+    document.getElementById("intro-box").style.display = "block";
+}
+
+// Function to hide the introduction box when the hover is removed
+function hideIntro() {
+ 
+    document.getElementById("intro-box").style.display = "none";
+}
